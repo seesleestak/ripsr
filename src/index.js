a8fa7ffe2b73f86bb12017f51c94150da3c337b9
Calvin Cieslak <calvin.cieslak@hixme.com>
2018-02-15 09:47:58 -0800

    Confirmation flow

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 
 const inquirer = require('inquirer')
+const chalk = require('chalk')
 const { exec } = require('child_process')
+
+let searchTerm = null
+let replaceTerm = null
 
 const prompts = [
   {
@@ -13,9 +17,63 @@ const prompts = [
     name: 'replace',
     type: 'input',
     message: 'Replace term:',
+  },
+  {
+    name: 'confirmSearch',
+    type: 'confirm',
+    message: 'Search results acceptable?'
+  },
+  {
+    name: 'confirmExecute',
+    type: 'confirm',
+    message: 'Execute search and replace?'
   }
 ]
 
-inquirer.prompt(prompts).then(({ search, replace }) => {
-  exec(`rg -l ${search} | xargs sed -i '' 's|${search}|${replace}|g'`)
-})
+function search() {
+  inquirer.prompt(prompts[0])
+    .then(({ search }) => {
+      exec(
+        `rg -l ${search}`,
+        (err, stdout, stderr) => {
+          console.log(chalk.magenta(stdout))
+          if (err) {
+            console.log('err --- ', err)
+            return 
+          } else {
+            searchTerm = search
+            confirmSearch()
+          }
+        }
+      )
+    })
+}
+
+function confirmSearch() {
+  inquirer.prompt(prompts[2])
+    .then(() => {
+      replace()
+    })
+}
+
+function replace() {
+  inquirer.prompt(prompts[1])
+    .then(({ replace }) => {
+      replaceTerm = replace
+      confirmSearchReplace()
+    })
+}
+
+function confirmSearchReplace() {
+  inquirer.prompt(prompts[3])
+    .then(() => {
+      exec(
+        `rg -l ${searchTerm} | xargs sed -i '' 's|${searchTerm}|${replaceTerm}|g'`,
+        (err) => {
+          err && console.log('err --- ', err)
+        }
+      )
+    })
+}
+
+search()
